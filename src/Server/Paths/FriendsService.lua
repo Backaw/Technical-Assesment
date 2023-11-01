89c8e03ad772915e2405a28c34ec79a261ec9561
Amani M <51408483+ambonimp@users.noreply.github.com>
2023-11-01 12:30:46 -0400

    Fix issues

--- src/Server/Paths/FriendsService.lua
+++ src/Server/Paths/FriendsService.lua
@@ -3,18 +3,19 @@ local FriendsService = {}
 local Players = game:GetService("Players")
 local ServerScriptService = game:GetService("ServerScriptService")
 local Paths = require(ServerScriptService.Paths)
-local PlayerService = require(Paths.services.PlayersService)
-local QuestService = require(Paths.services.QuestService)
+local PlayerService = require(Paths.Services.PlayersService)
+local QuestService = require(Paths.Services.QuestService)
 
 FriendsService.loadPlayer = PlayerService.promisifyLoader(function(player)
 	for _, otherPlayer in pairs(Players:GetPlayers()) do
-		if player ~= otherPlayer and otherPlayer:IsFriendsWith(player.UserId) then
-			QuestService.incrementStat(player, "InvitedFriends", tostring(otherPlayer.UserId))
-
-			PlayerService.onLoad(otherPlayer, function()
-				QuestService.incrementStat(otherPlayer, "InvitedFriends", tostring(player.UserId))
-			end)
-		end
+		pcall(function()
+			if player ~= otherPlayer and otherPlayer:IsFriendsWith(player.UserId) then
+				QuestService.incrementStat(player, "InvitedFriends", tostring(otherPlayer.UserId))
+				PlayerService.onLoad(otherPlayer, function()
+					QuestService.incrementStat(otherPlayer, "InvitedFriends", tostring(player.UserId))
+				end)
+			end
+		end)
 	end
 end, "Friends")
 
