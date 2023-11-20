523985acd187e3ad797112c1abe464fd6f2a04c6
Amani M <51408483+ambonimp@users.noreply.github.com>
2023-11-20 13:35:40 -0500

    Add pointer gizmo

--- src/Shared/Utils/DebugUtil.lua
+++ src/Shared/Utils/DebugUtil.lua
@@ -7,7 +7,7 @@ function DebugUtil.previewCFrame(cframe: CFrame, size: Vector3?, color: Color3?)
 	local preview = Instance.new("WedgePart")
 	preview.Size = size or Vector3.new(1, 1, 1)
 	preview.CFrame = cframe
-	preview.Color = color or Color3.new(255, 255, 255)
+	preview.Color = color or Color3.fromRGB(0, 0, 0)
 	preview.Anchored = true
 	preview.CanTouch = false
 	preview.CanQuery = false
@@ -15,6 +15,19 @@ function DebugUtil.previewCFrame(cframe: CFrame, size: Vector3?, color: Color3?)
 	preview.Parent = workspace
 
 	return preview
+end
+
+function DebugUtil.pointerGizmo(parent: BasePart, color: Color3?, length: number?)
+	local gizmo = Instance.new("LineHandleAdornment") -- Points in direction of velocity
+	gizmo.Adornee = parent
+	gizmo.AlwaysOnTop = true
+	gizmo.Color3 = color or Color3.fromRGB(0, 180, 255)
+	gizmo.Parent = parent
+	gizmo.Thickness = 3
+	gizmo.Length = length or 5
+	gizmo.Visible = true
+
+	return gizmo
 end
 
 function DebugUtil.truncateVector3(vector: Vector3, precision: number)
